7faff573cf5750a328cdd0cd455bdca54cda624e
damian <damiangalvan66@gmail.com>
2023-02-23 12:08:13 -0600

    Remove 'tasks' table from the schema.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,11 +9,3 @@ CREATE TABLE IF NOT EXISTS `jdbc_database`.`users`
     PRIMARY KEY (`id`),
     UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
 );
-
-CREATE TABLE IF NOT EXISTS `jdbc_database`.`tasks`
-(
-    `id`          INT         NOT NULL AUTO_INCREMENT,
-    `name`        VARCHAR(45) NULL,
-    `description` VARCHAR(45) NULL,
-    PRIMARY KEY (`id`)
-);
