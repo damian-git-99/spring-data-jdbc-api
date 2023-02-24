c2d9bc2a9cde5d06222e4fdca59e1d259ce85005
damian <damiangalvan66@gmail.com>
2023-02-24 10:20:46 -0600

    Commit message: Add tasks field to User model and include tasks in UserRepositoryJDBC queries

--- src/main/java/com/github/damian_git_99/user/model/User.java
+++ src/main/java/com/github/damian_git_99/user/model/User.java
@@ -5,15 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Builder
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class User {
-
     private Long id;
     private String username;
     private String email;
     private String phone;
-
+    @Builder.Default
+    private List<Task> tasks = new ArrayList<>();
 }
--- src/main/java/com/github/damian_git_99/user/repositories/UserRepositoryJDBC.java
+++ src/main/java/com/github/damian_git_99/user/repositories/UserRepositoryJDBC.java
@@ -4,13 +4,17 @@ package com.github.damian_git_99.user.repositories;
 import com.github.damian_git_99.user.model.Task;
 import com.github.damian_git_99.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.*;
 
 @Repository
 public class UserRepositoryJDBC implements UserRepository {
@@ -23,14 +27,36 @@ public class UserRepositoryJDBC implements UserRepository {
     }
 
     @Override
-    public Optional<User> findUserById(Long id) {
-        try {
-            String query = "SELECT * FROM users WHERE users.id = ?";
-            User user = jdbc.queryForObject(query, BeanPropertyRowMapper.newInstance(User.class), id);
-            return Optional.ofNullable(user);
-        } catch (IncorrectResultSizeDataAccessException e) {
-            return Optional.empty();
-        }
+    public Optional<User> findUserById(Long userId) {
+        String sql = "SELECT u.id, u.username, u.email, u.phone, t.id as task_id, t.name as task_name, t.description as task_description " +
+                "FROM users u " +
+                "JOIN tasks t ON u.id = t.user_id " +
+                "WHERE u.id = ?";
+
+        User user = jdbc.query(sql, new Object[]{userId}, new int[]{Types.INTEGER}, rs -> {
+            User u = null;
+            while (rs.next()) {
+                if (u == null) {
+                    u = User.builder()
+                            .id(rs.getLong("id"))
+                            .username(rs.getString("username"))
+                            .email(rs.getString("email"))
+                            .phone(rs.getString("phone"))
+                            .build();
+                }
+
+                Task task = Task.builder()
+                        .id(rs.getLong("task_id"))
+                        .name(rs.getString("task_name"))
+                        .description(rs.getString("task_description"))
+                        .build();
+
+                u.getTasks().add(task);
+            }
+            return u;
+        });
+
+        return Optional.ofNullable(user);
     }
 
     @Override
@@ -53,8 +79,34 @@ public class UserRepositoryJDBC implements UserRepository {
 
     @Override
     public List<User> findAllUsers() {
-        String query = "SELECT * FROM users";
-        return jdbc.query(query, BeanPropertyRowMapper.newInstance(User.class));
+        String sql = "SELECT u.id, u.username, u.email, u.phone, t.id AS task_id, t.name AS task_name, t.description AS task_description " +
+                "FROM users u LEFT JOIN tasks t ON u.id = t.user_id ORDER BY u.id";
+        return jdbc.query(sql, (ResultSetExtractor<List<User>>) rs -> {
+            Map<Long, User> userMap = new HashMap<>();
+            while (rs.next()) {
+                Long userId = rs.getLong("id");
+                User user = userMap.get(userId);
+                if (user == null) {
+                    user = User.builder()
+                            .id(userId)
+                            .username(rs.getString("username"))
+                            .email(rs.getString("email"))
+                            .phone(rs.getString("phone"))
+                            .tasks(new ArrayList<>())
+                            .build();
+                    userMap.put(userId, user);
+                }
+                long taskId = rs.getLong("task_id");
+                if (taskId > 0) {
+                    Task task = new Task();
+                    task.setId(taskId);
+                    task.setName(rs.getString("task_name"));
+                    task.setDescription(rs.getString("task_description"));
+                    user.getTasks().add(task);
+                }
+            }
+            return new ArrayList<>(userMap.values());
+        });
     }
 
     @Override
@@ -75,6 +127,11 @@ public class UserRepositoryJDBC implements UserRepository {
         String query = "INSERT INTO tasks (name, description, user_id) VALUES (?,?,?)";
         int result = jdbc.update(query, task.getName(), task.getDescription(), task.getUserId());
         return result == 1;
+    }
+
+    private List<Task> findTasksByUserId(Long userId) {
+        String query = "SELECT * FROM tasks WHERE user_id =?";
+        return jdbc.query(query, BeanPropertyRowMapper.newInstance(Task.class), userId);
     }
 
 }
